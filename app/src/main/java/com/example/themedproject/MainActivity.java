bb97b6d6172d17eac07044cc1ba19a26d34aec00
NabeelMehar <nabeel.mehar.khan@gmail.com>
2020-03-18 18:11:52 +0530

    hELLO wORLD dELETED cOMMIT

--- app/src/main/java/com/example/themedproject/MainActivity.java
+++ app/src/main/java/com/example/themedproject/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        aboutApp.setText("Hello World");
     }
 
 
