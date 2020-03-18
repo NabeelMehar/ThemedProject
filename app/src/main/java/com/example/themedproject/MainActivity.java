fbd18c649ea9c3b3f0f2006d105aa1d163636de3
NabeelMehar <nabeel.mehar.khan@gmail.com>
2020-03-18 18:07:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/themedproject/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.themedproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.horaapps.liz.ui.ThemedIcon;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.about_app_title) TextView aboutApp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+
+
+//    @OnClick(R.id.about_app_title)
+//    public void onReport(){
+//        Toast.makeText(this, "Clicked Title", Toast.LENGTH_SHORT).show();
+//    }
+}
